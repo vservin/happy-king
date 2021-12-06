97de0dd569910adb047143c75e58f0ab23483058
valltw <valltw@gmail.com>
2021-12-05 18:42:26 -0600

    Added what to do depending on the given card and a nice loader

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let deckId = '';
 let remainingCards ='52';
+let value ='';
+let suit='';
 
 function getNewDeckOfCards() {
   return fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
@@ -17,6 +19,7 @@ document.getElementById('card').addEventListener('click',function(){
   if(remainingCards === 0){
     document.getElementById('card').src = 'https://www.latercera.com/resizer/GU5p9M9URqdx51nSyBWjIoO2Vzo=/900x600/smart/arc-anglerfish-arc2-prod-copesa.s3.amazonaws.com/public/OOPGZQS4GNB6XJ4TYPEOST7J6E.jpg';
     setTimeout(() => location.reload(),5000);
+    document.getElementById('todo').innerHTML = ('Wait, we need to find you another deck...  <div class="loader"></div>');
     return; 
   }
 
@@ -25,8 +28,9 @@ document.getElementById('card').addEventListener('click',function(){
   }
   else {
     drawACardFromDeck(deckId).then((dataFromCard) =>{
-      console.log(dataFromCard);
+      console.log(dataFromCard.cards[0].value,dataFromCard.cards[0].suit);
       remainingCards=dataFromCard.remaining;
+      whatToDo(dataFromCard.cards[0].value,dataFromCard.cards[0].suit);
       document.getElementById('card').src = dataFromCard.cards[0].image ;
     });
   }
@@ -43,11 +47,29 @@ function newDeckAndDrawOneCard(){
     // console.log(dataFromCard.cards[0].image);
     // let img = document.createElement('img'); 
     // img.src = dataFromCard.cards[0].image; 
+    console.log(dataFromCard.cards[0].value);
+    whatToDo(dataFromCard.cards[0].value,dataFromCard.cards[0].suit);
     document.getElementById('card').src = dataFromCard.cards[0].image ;
   });
 }
 
+function whatToDo(value,suit){
 
+  if(value === 'ACE' || value === 'QUEEN' || value === 'KING' || value === 'JACK' ){
+    document.getElementById('todo').innerHTML = ('Everybody drinks');
+    return;
+  }
+
+if (suit === 'CLUBS' || suit === 'SPADES'){
+  document.getElementById('todo').innerHTML = ('Send '+value+' drinks');
+
+}
+else{
+  document.getElementById('todo').innerHTML = ('Take '+ value +' drinks');
+}
+
+
+}
 
 
 // function hola() {
